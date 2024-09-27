bb5b9566fc1d7aa05f6f83904a1b04a7f93e9d11
Ritu Prajapati <130266066+Ritu-Prajapati@users.noreply.github.com>
2024-09-27 15:05:30 +0530

    Add files via upload

---
+++ Problem3.py
@@ -0,0 +1,11 @@
+# using pip install wikipedia
+import wikipedia
+result = wikipedia.page("World")
+print(result.summary)
+
+# using pip install pyttsx3
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hi I am Ritu")
+engine.runAndWait()
