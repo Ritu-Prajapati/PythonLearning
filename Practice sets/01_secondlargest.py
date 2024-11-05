c02376303cf31525546630acd1ae88422b45df70
Ritu Prajapati <130266066+Ritu-Prajapati@users.noreply.github.com>
2024-11-05 15:41:59 +0530

    Update 01_secondlargest.py

--- Practice sets/01_secondlargest.py
+++ Practice sets/01_secondlargest.py
@@ -2,6 +2,7 @@ arr= [5, 5, 4, 1,3,2]
 n = len(arr)
 
 def secondlargest(arr,n):
+    #giving both the variable as -ve infinity such that arr[] values can easily replaced with largest +ve values
     first_lar = float("-inf")
     second_lar = float("-inf")
 
@@ -9,7 +10,7 @@ def secondlargest(arr,n):
         if arr[i] > first_lar:
             second_lar = first_lar
             first_lar = arr[i]
-            
+        # arr[i] != helps in not considering duplicates value    
         elif arr[i]> second_lar and arr[i] != first_lar:
             second_lar = arr[i]
     print(second_lar)
